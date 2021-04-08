c065bd80f2e48df1fd39b3a4138fa18a1c531170
Mario Tarosso <mariojgt2@gmmail.com>
2021-04-08 12:02:59 +0100

    Quick update in the composer file

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Mariojgt\Biscotto\Controllers\BiscottoContoller;
 
 // Standard
-Route::group([
-    'middleware' => ['web']
-], function () {
-    // example page not required to be login
-    Route::post('/biscotto/savecookie', [BiscottoContoller::class, 'index'])->name('biscotto.savecookie');
-});
+// Route::group([
+//     'middleware' => ['web']
+// ], function () {
+//     // example page not required to be login
+//     Route::post('/biscotto/savecookie', [BiscottoContoller::class, 'index'])->name('biscotto.savecookie');
+// });
