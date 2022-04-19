996524cbb2613a87d0438a9df1b67bf595d6c01a
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2022-04-19 10:37:41 +0100

    Cookie Floater fix
    added more style to the popup button.

--- src/views/components/cookie_floater.blade.php
+++ src/views/components/cookie_floater.blade.php
@@ -5,35 +5,48 @@
         height:60px; */
         bottom: 40px;
         left: 40px;
-        background-color: #0C9;
+        background-color: black;
         color: #FFF;
-        border-radius: 50px;
+        border: 2px solid white;
+        padding: 1px;
+        border-radius: 9px;
         text-align: center;
     }
-
 
     .my-float {
         margin-left: auto;
         margin-right: auto;
     }
+
 </style>
 
 <script>
     function onCookiePopup(params) {
         let setting = document.querySelector('#cookie_popup');
-            setting.style.opacity = "1";
+        setting.style.opacity = "1";
     }
 
     function offCookiePopup(params) {
         let setting = document.querySelector('#cookie_popup');
-            setting.style.opacity = "{{ config('biscotto.biscotto_opacity') }}";
+        setting.style.opacity = "{{ config('biscotto.biscotto_opacity') }}";
     }
 
+    function enableCookie() {
+        // Find the cookie
+        let cookie = document.querySelector('#cookie_popup');
+        // Make the cookie visible
+        cookie.style.display = "block";
+        cookie.style.opacity = "0.2";
+    }
+
+    setTimeout(() => {
+        enableCookie();
+    }, 1000);
 </script>
 
 <a href="#" class="float" onclick="showCookie()" onmouseover="onCookiePopup()" onmouseout="offCookiePopup()"
-    id="cookie_popup">
+    id="cookie_popup" style="display: none;">
     <div class="my-float">
-        <h1 >🍪</h1>
+        <h1>🍪</h1>
     </div>
 </a>
